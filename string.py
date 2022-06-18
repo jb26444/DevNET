4a7b5f5961707016102a9bf619cb92df22d4bbca
Jan Blahuta <jan.blahuta@gmail.com>
2022-06-18 07:52:58 +0200

    String operation

---
+++ string.py
@@ -0,0 +1,36 @@
+#https://www.digitalocean.com/community/tutorials/an-introduction-to-string-functions-in-python-3
+
+
+a = "hello Python"
+
+#Print a string
+print(a)
+
+#multiply string
+print((a +" ")* 3)
+
+#print a 3rd letter from a string
+print(a[2])
+
+#print a first letter from the end of the string
+print(a[-1])
+
+#print all in caps
+print(a.upper())
+
+#print all lowercase
+print(a.lower())
+
+#print a third letter uppercase
+print(a.upper()[2])
+
+#pring a lenght of the string
+print(len(a))
+
+#join statement - it will join two string but into one in cop style
+print(" ".join(a))
+print(" ".join(a.upper())) # joing and all in upper case style.
+
+
+
+
