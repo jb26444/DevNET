6c0a00fb05def186954dabed7f9f1cc934118c4f
Jan Blahuta <jan.blahuta@gmail.com>
2022-06-19 08:30:34 +0200

    list and dic operation

---
+++ list_dic.py
@@ -0,0 +1,19 @@
+list_a = ["apple", "oranges", "car", 11, 1920,]
+dic_a = {"car":"BMW", "colour":"blue", "engine":2.0}
+
+#print list
+print(list_a)
+
+#print the lenght of list
+print(len(list_a))
+
+#print a value from the list
+print(list_a[1])
+
+#print dictionary
+print(dic_a)
+print(dic_a.values())
+print(dic_a.keys)
+
+for key in dic_a:
+    print(dic_a[car])
